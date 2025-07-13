b0c01e9f92c70991a95d04b74e68f5c850ef5bfd
ilyes <ismailbenameur2004@gmail.com>
2025-07-13 20:07:43 +0200

    feat: implemented reset filters feature - remove todo

--- resources/views/components/date-picker.blade.php
+++ resources/views/components/date-picker.blade.php
@@ -6,6 +6,7 @@
     datepicker-format="yyyy-mm-dd"
     date-rangepicker class="flex items-center"
     datepicker-buttons
+    datepicker-autohide
 >
     <input
         id="datepicker-range-start"
@@ -14,7 +15,7 @@
         class="w-full rounded-md border-gray-300 dark:border-gray-700 bg-white/80 dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-400 focus:outline-none px-3 py-2 text-sm placeholder-gray-400 dark:placeholder-gray-500"
         placeholder="Select date start"
     />
-    <span class="mx-4 text-gray-500">to</span>
+    <span class="mx-4 text-white">to</span>
     <input
         id="datepicker-range-end"
         name="end"
@@ -29,6 +30,7 @@
     datepicker-format="yyyy-mm-dd"
     datepicker-buttons
     datepicker-autoselect-today
+    datepicker-autohide
     type="text"
     @if($id) id="{{ $id }}" @endif
     @if($name) name="{{ $name }}" @endif
