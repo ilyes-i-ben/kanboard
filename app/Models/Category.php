09c59096620a35299bf5a4dcde236e2d98ad0bcb
ilyes <ismailbenameur2004@gmail.com>
2025-07-15 23:20:43 +0200

    db: added categories table and foreign key constraints to cards

---
+++ app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name',
+        'board_id',
+    ];
+
+    public function board(): BelongsTo
+    {
+        return $this->belongsTo(Board::class);
+    }
+
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class);
+    }
+}
+
