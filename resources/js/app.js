e1f44b0bb003a4b1adac33b2af455824299eafdd
ilyes <ismailbenameur2004@gmail.com>
2025-07-15 23:23:34 +0200

    assume-unchanged: pushing app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,10 @@ import './bootstrap';
 import './theme.js';
 import 'tinymce/tinymce';
 import 'tinymce/skins/ui/oxide/skin.min.css';
-import 'tinymce/skins/content/default/content.min.css';
+// import 'tinymce/skins/content/default/content.min.css';
 // TODO: remove the body margin 1rem since it's causing glitch in dark mode.
-import 'tinymce/skins/content/default/content.css';
+// import 'tinymce/skins/content/default/content.css';
+import '../css/content.css';
 import 'tinymce/icons/default/icons';
 import 'tinymce/themes/silver/theme';
 import 'tinymce/models/dom/model';
@@ -19,6 +20,13 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+import './list-view.js';
+import './lists/index.js';
+import './cards/index.js';
+import './boards/index.js';
+
+import './toast.js';
+import './confirmationModal.js';
 
 Alpine.plugin(sort)
 window.Alpine = Alpine;
