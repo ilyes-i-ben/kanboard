8b95254cb8e5b53dbf8c3f1bb1ad7cee2f342016
ilyes <ismailbenameur2004@gmail.com>
2025-07-17 18:31:13 +0200

    feat: board cards can be exported to .ICS file format

---
+++ app/Http/Controllers/CardCalendarController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Board;
+use App\Services\CardService;
+use Illuminate\Http\Request;
+
+class CardCalendarController extends Controller
+{
+    public function __construct(
+        private readonly CardService $cardService,
+    ) {
+    }
+
+    public function boardCalendar(Board $board)
+    {
+        $cards = $board->lists()->with('cards')->get()->pluck('cards')->flatten();
+
+        $ics = $this->cardService->generateICS($cards);
+
+        return response($ics, 200)
+            ->header('Content-Type', 'text/calendar')
+            ->header('Content-Disposition', 'attachment; filename="board-'.$board->id.'.ics"');
+    }
+}
