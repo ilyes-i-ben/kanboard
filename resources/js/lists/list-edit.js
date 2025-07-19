0f106db9d362354376773d7d3af469fd26ecdccc
ilyes <ismailbenameur2004@gmail.com>
2025-07-19 17:21:23 +0200

    chore: fix green background of confirm + list containers

--- resources/js/lists/list-edit.js
+++ resources/js/lists/list-edit.js
@@ -57,7 +57,12 @@ function addEmptied(listId) {
     const container = document.getElementById(`list-cards-container-${listId}`);
     if (container) {
         container.innerHTML = `
-            <div class="empty-list flex flex-col items-center justify-center text-gray-800 dark:text-gray-400 py-8 text-sm">
+            <div
+                class="empty-list flex flex-col items-center justify-center h-full text-gray-800 dark:text-gray-400 py-8 text-sm"
+                data-no-sort="true"
+                x-sortable-ignore
+            >
+
                 <svg class="w-10 h-10 mb-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" data-slot="icon">
                   <path fill-rule="evenodd" d="m6.72 5.66 11.62 11.62A8.25 8.25 0 0 0 6.72 5.66Zm10.56 12.68L5.66 6.72a8.25 8.25 0 0 0 11.62 11.62ZM5.105 5.106c3.807-3.808 9.98-3.808 13.788 0 3.808 3.807 3.808 9.98 0 13.788-3.807 3.808-9.98 3.808-13.788 0-3.808-3.807-3.808-9.98 0-13.788Z" clip-rule="evenodd"></path>
                 </svg>
